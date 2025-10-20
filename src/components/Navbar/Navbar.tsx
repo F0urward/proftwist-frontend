0b8ec6a5968d31972cae969b964900e0b37ba378
LizaSemyan <liza777sem@icloud.com>
2025-10-20 08:33:31 +0300

    feat: add active page to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
-import { Link as RouterLink } from "react-router-dom";
+import { Link as RouterLink, NavLink } from "react-router-dom";
 import RoadmapsDropdown from "../RoadmapsDropdown/RoadmapsDropdown";
 
 type NavbarProps = {
@@ -7,107 +7,107 @@ type NavbarProps = {
 };
 
 const Navbar = ({ isAuth }: NavbarProps) => {
-  return (
-    <AppBar 
-        position="static" 
-        sx={{ 
-            bgcolor: "background.paper",
-        }}
-    >
-        <Toolbar 
-            sx={{
-                display: "flex", 
-                justifyContent: "space-between",
-                px: { xs: 3, md: 20 }, 
+    return (
+        <AppBar 
+            position="static" 
+            sx={{ 
+                bgcolor: "background.paper",
             }}
         >
-
-            <Typography
-                variant="h6"
-                component={RouterLink}
-                to="/roadmaps"
-                sx={{ 
-                    fontFamily: '"TDAText"',
-                    textDecoration: "none", 
-                    fontWeight: 700,
-                    backgroundImage: "linear-gradient(90deg, #BC57FF 0%, #FF4DCA 100%)",
-                    backgroundClip: "text",
-                    WebkitBackgroundClip: "text",
-                    color: "transparent",
-                    WebkitTextFillColor: "transparent",
+            <Toolbar 
+                sx={{
+                    display: "flex", 
+                    justifyContent: "space-between",
+                    px: { xs: 3, md: 20 }, 
                 }}
             >
-                ProfTwist
-            </Typography>
 
-            {!isAuth && (
-                <>
-                <Box
-                    sx={{
-                        flex: 1,
-                        display: "flex",
-                        justifyContent: "center",
-                        gap: 3,
+                <Typography
+                    variant="h6"
+                    component={RouterLink}
+                    to="/roadmaps"
+                    sx={{ 
+                        fontFamily: '"TDAText"',
+                        textDecoration: "none", 
+                        fontWeight: 700,
+                        backgroundImage: "linear-gradient(90deg, #BC57FF 0%, #FF4DCA 100%)",
+                        backgroundClip: "text",
+                        WebkitBackgroundClip: "text",
+                        color: "transparent",
+                        WebkitTextFillColor: "transparent",
                     }}
                 >
-                    <RoadmapsDropdown />
-                    <Button
-                        variant="text"
-                        component={RouterLink} 
-                        to="/materials" 
-                    >
-                        Материалы
-                    </Button>
-                    <Button
-                        variant="text"
-                        component={RouterLink} 
-                        to="/chats" 
-                    >
-                        Чаты
-                    </Button>
-                    <Button
-                        variant="text"
-                        component={RouterLink} 
-                        to="/"
-                    >
-                        Create
-                    </Button>
-                    <Button 
-                        variant="text"
-                        component={RouterLink}
-                        to="/view"
-                    >
-                        View
-                    </Button>
-                </Box>
-                
-                <Box
-                    sx={{
-                        display: "flex",
-                        gap: 3,
-                    }}
-                >
-                    <Button
-                        variant="text"
-                        component={RouterLink}
-                        to="/login"
-                    >
-                        Войти
-                    </Button>
+                    ProfTwist
+                </Typography>
 
-                    <Button
-                        variant="contained"
-                        component={RouterLink}
-                        to="/signup"
+                {!isAuth && (
+                    <>
+                    <Box
+                        sx={{
+                            flex: 1,
+                            display: "flex",
+                            justifyContent: "center",
+                            gap: 3,
+                        }}
                     >
-                        Зарегистрироваться
-                    </Button>
-                </Box>
-            </>
-        )}
-      </Toolbar>
-    </AppBar>
-  );
+                        <RoadmapsDropdown />
+                        <Button
+                            variant="text"
+                            component={NavLink} 
+                            to="/materials" 
+                        >
+                            Материалы
+                        </Button>
+                        <Button
+                            variant="text"
+                            component={NavLink} 
+                            to="/chats" 
+                        >
+                            Чаты
+                        </Button>
+                        <Button
+                            variant="text"
+                            component={NavLink} 
+                            to="/"
+                        >
+                            Create
+                        </Button>
+                        <Button 
+                            variant="text"
+                            component={NavLink}
+                            to="/view"
+                        >
+                            View
+                        </Button>
+                    </Box>
+                    
+                    <Box
+                        sx={{
+                            display: "flex",
+                            gap: 3,
+                        }}
+                    >
+                        <Button
+                            variant="text"
+                            component={RouterLink}
+                            to="/login"
+                        >
+                            Войти
+                        </Button>
+
+                        <Button
+                            variant="contained"
+                            component={RouterLink}
+                            to="/signup"
+                        >
+                            Зарегистрироваться
+                        </Button>
+                    </Box>
+                </>
+            )}
+        </Toolbar>
+        </AppBar>
+    );
 };
 
 export default Navbar;
