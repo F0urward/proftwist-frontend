cab7fb902fdf4a84215b83a08c4267ee021a4215
LizaSemyan <liza777sem@icloud.com>
2025-11-12 19:06:30 +0300

    feat: show profile info

--- src/components/ProfileView/ProfileView.tsx
+++ src/components/ProfileView/ProfileView.tsx
@@ -9,24 +9,89 @@ import EditIcon from "@mui/icons-material/Edit";
 import EmailIcon from "@mui/icons-material/Email";
 import PersonIcon from "@mui/icons-material/Person";
 import TextInput from "../TextInput/TextInput";
+import { profileSchema, ProfileFormData } from "../../utils/entrySchemas";
+import { User } from "../../types/auth";
+import { useNotification } from "../Notification/Notification";
+import { authService } from "../../api";
+import { useEffect } from "react";
+import { useAppSelector, RootState } from "../../store";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const ProfileView = () => {
-  const [open, setOpen] = useState(false);
-  const fileRef = useRef<HTMLInputElement | null>(null);
+    const user = useAppSelector((state: RootState) => state.auth.user);
+    const [open, setOpen] = useState(false);
+    const fileRef = useRef<HTMLInputElement | null>(null);
+    const { showNotification, Notification } = useNotification();
 
-  const openDialog = () => {
-    setOpen(true);
-  };
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset,
+        watch,
+    } = useForm<ProfileFormData>({
+        resolver: zodResolver(profileSchema),
+        defaultValues: {
+        username: "",
+        email: "",
+        password: "",
+        passwordRepeat: "",
+        },
+    });
 
+    const openDialog = () => {
+        setOpen(true);
+    };
 
-  const handlePickAvatar = (e: ChangeEvent<HTMLInputElement>) => {
-  };
+    const handlePickAvatar = async (e: ChangeEvent<HTMLInputElement>) => {
+        if (!e.target.files?.length) return;
+        const file = e.target.files[0];
+        try {
+            await authService.uploadAvatar(file);
+            showNotification("Аватар успешно обновлён", "success");
+        } catch {
+            showNotification("Ошибка загрузки аватара", "error");
+        }
+    };
 
-  const handleSave = async () => {
-    setOpen(false);
-  };
+    const onSubmit = async (data: ProfileFormData) => {
+        try {
+            const result = await authService.update({
+                nickname: data.username,
+                email: data.email,
+                password: data.password || undefined,
+                new_password: data.passwordRepeat || undefined,
+            });
+            console.log(result);
+            showNotification("Профиль успешно обновлён", "success");
+            setOpen(false);
+        } catch (err) {
+            console.error(err);
+            showNotification("Не удалось сохранить изменения", "error");
+        }
+    };
+    
+    useEffect(() => {
+        if (user) return;
+        async function getMe() {
+            try {
+                const me = await authService.getMe();
+                reset({
+                    username: me.username || "",
+                    email: me.email || "",
+                    password: "",
+                    passwordRepeat: "",
+                });
+            } catch {
+                showNotification("Не удалось загрузить профиль", "error");
+            } finally {
+            }
+        };
+        getMe();
+    }, [user]);
 
-  return (
+    return (
         <Paper
             variant="outlined"
             sx={{
@@ -53,7 +118,7 @@ const ProfileView = () => {
             <Box sx={{ p: 3 }}>
                 <Grid container spacing={3} alignItems="center">
                     <Avatar
-                        src="/static/images/avatar/1.jpg"
+                        src={user?.image || "/static/images/avatar/1.jpg"}
                         sx={{
                             width: 96,
                             height: 96,
@@ -65,11 +130,11 @@ const ProfileView = () => {
                     <Stack spacing={1.5}>
                         <Stack direction="row" spacing={1} alignItems="center">
                             <PersonIcon fontSize="small" />
-                            <Typography>Nickname</Typography>
+                            <Typography>{user?.username}</Typography>
                         </Stack>
                         <Stack direction="row" spacing={1} alignItems="center">
                             <EmailIcon fontSize="small" />
-                            <Typography>Email</Typography>
+                            <Typography>{user?.email}</Typography>
                         </Stack>
                     </Stack>
                 </Grid>
@@ -77,74 +142,85 @@ const ProfileView = () => {
 
             <Dialog open={open} onClose={() => setOpen(false)} maxWidth="sm" fullWidth>
                 <DialogTitle>Настройки профиля</DialogTitle>
-                <DialogContent sx={{ p: 3, pb: 0 }}>
-                    <Stack spacing={3}>
-                        <Stack direction="row" spacing={2} alignItems="center">
-                            <Avatar
-                                src="/static/images/avatar/1.jpg"
-                                sx={{
-                                    width: 72, height: 72, fontSize: 24,
-                                    bgcolor: alpha("#BC57FF", 0.15),
-                                    border: "1px solid rgba(255,255,255,.12)",
-                                }}
-                            >
-                            </Avatar>
+                <Box component="form" onSubmit={handleSubmit(onSubmit)}>
+                    <DialogContent sx={{ p: 3, pb: 0 }}>
+                        <Stack spacing={3}>
+                            <Stack direction="row" spacing={2} alignItems="center">
+                                <Avatar
+                                    src={user?.image}
+                                    sx={{
+                                        width: 72, height: 72, fontSize: 24,
+                                        bgcolor: alpha("#BC57FF", 0.15),
+                                        border: "1px solid rgba(255,255,255,.12)",
+                                    }}
+                                >
+                                </Avatar>
+                                <input
+                                    ref={fileRef}
+                                    type="file"
+                                    accept="image/*"
+                                    hidden
+                                    onChange={handlePickAvatar}
+                                />
+                                <Button
+                                    variant="text"
+                                    startIcon={<PhotoCamera />}
+                                    onClick={() => fileRef.current?.click()}
+                                >
+                                    Загрузить аватар
+                                </Button>
+                            </Stack>
 
-                            <input
-                                ref={fileRef}
-                                type="file"
-                                accept="image/*"
-                                hidden
-                                onChange={handlePickAvatar}
+                            <TextInput
+                                label="Никнейм"
+                                placeholder="Введите никнейм"
+                                {...register("username")}
+                                error={!!errors.username}
+                                helperText={errors.username?.message}
                             />
-                            <Button
-                                variant="text"
-                                startIcon={<PhotoCamera />}
-                                onClick={() => fileRef.current?.click()}
-                            >
-                                Загрузить аватар
-                            </Button>
+
+                            <TextInput
+                                label="Email"
+                                type="email"
+                                placeholder="Введите почту"
+                                {...register("email")}
+                                error={!!errors.email}
+                                helperText={errors.email?.message}
+                            />
+
+                            <Divider sx={{ borderColor: "rgba(255,255,255,.08)" }} />
+                                
+                            <TextInput
+                                label="Новый пароль"
+                                type="password"
+                                placeholder="Введите пароль"
+                                {...register("password")}
+                                error={!!errors.password}
+                                helperText={errors.password?.message}
+                            />
+
+                            <TextInput
+                                label="Повторите пароль"
+                                type="password"
+                                placeholder="Повторите пароль"
+                                {...register("passwordRepeat")}
+                                error={!!errors.passwordRepeat}
+                                helperText={errors.passwordRepeat?.message}
+                            />
                         </Stack>
-
-                        <TextInput
-                            label="Ник"
-                            value="Текущий никнейм" 
-                        />
-
-                        <TextInput
-                            label="Почта"
-                            type="email"
-                            value="Текущий email"
-                        />
-
-                        <Divider sx={{ borderColor: "rgba(255,255,255,.08)" }} />
-                            
-                        <TextInput
-                            label="Текущий пароль"
-                            type="password"
-                        />
-
-                        <TextInput
-                            label="Новый пароль"
-                            type="password"
-                        />
-
-                        <TextInput
-                            label="Подтверждение пароля"
-                            type="password"
-                        />
-                    </Stack>
-                </DialogContent>
-                <DialogActions sx={{ p: 3, gap: 1 }}>
-                    <Button variant="text" onClick={() => setOpen(false)}>Отмена</Button>
-                    <Button
-                        variant="contained"
-                        onClick={handleSave}
-                    >
-                        Сохранить
-                    </Button>
-                </DialogActions>
+                    </DialogContent>
+                    <DialogActions sx={{ p: 3, gap: 1 }}>
+                        <Button variant="text" onClick={() => setOpen(false)}>Отмена</Button>
+                        <Button
+                            variant="contained"
+                            type="submit"
+                        >
+                            Сохранить
+                        </Button>
+                    </DialogActions>
+                </Box>
             </Dialog>
+            { Notification }
         </Paper>
     );
 };
---
+++ src/utils/entrySchemas.ts
@@ -0,0 +1,68 @@
+import { z } from "zod";
+
+export const registerSchema = z
+  .object({
+    username: z
+      .string()
+      .min(3, "Никнейм должен содержать хотя бы 3 символа")
+      .max(20, "Никнейм максимально может содержать 20 символов")
+      .regex(
+        /^[a-zA-Z0-9_]+$/,
+        "Никнейм может содержать только буквы, цифры и нижнее подчеркивание",
+      )
+      .nonempty("Введите никнейм"),
+    email: z.email("Некорректный адрес почты").nonempty("Введите адрес почты"),
+    password: z
+      .string()
+      .min(8, "Пароль должен содержать хотя бы 8 символов")
+      .regex(/.*\d.*/, "Пароль должен содержать хотя бы одну цифру")
+      .nonempty("Введите пароль"),
+    passwordRepeat: z.string().nonempty("Пожалуйста, повторите пароль"),
+  })
+  .refine((data) => data.password === data.passwordRepeat, {
+    message: "Пароли должны совпадать",
+    path: ["passwordRepeat"], // Attach error to passwordRepeat field
+  });
+
+export type SignupFormData = z.infer<typeof registerSchema>;
+
+export const profileSchema = z
+  .object({
+    username: z
+      .string()
+      .min(3, "Никнейм должен содержать хотя бы 3 символа")
+      .max(20, "Максимум 20 символов")
+      .regex(/^[a-zA-Z0-9_]+$/, "Можно использовать только буквы, цифры и _")
+      .nonempty("Введите никнейм"),
+
+    email: z
+      .string()
+      .email("Некорректный адрес почты")
+      .nonempty("Введите адрес почты"),
+
+    password: z
+      .string()
+      .min(8, "Пароль должен содержать хотя бы 8 символов")
+      .regex(/.*\d.*/, "Пароль должен содержать хотя бы одну цифру")
+      .optional()
+      .or(z.literal("")),
+
+    passwordRepeat: z
+      .string()
+      .optional()
+      .or(z.literal("")),
+  })
+  .refine(
+    (data) => {
+      if (data.password && data.password !== "") {
+        return data.password === data.passwordRepeat;
+      }
+      return true;
+    },
+    {
+      message: "Пароли должны совпадать",
+      path: ["passwordRepeat"],
+    },
+  );
+
+export type ProfileFormData = z.infer<typeof profileSchema>;
